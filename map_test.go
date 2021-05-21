71d204900218b8b570bea48093eb77364cde0701
smartwalle <smartwalle@gmail.com>
2021-05-21 11:27:02 +0800

    添加 DJB Hash

--- map_test.go
+++ map_test.go
@@ -22,11 +22,63 @@ func BenchmarkMapBKDR_Set(b *testing.B) {
 	}
 }
 
+func BenchmarkMapDJB_Set(b *testing.B) {
+	var m = New(WithDJBHash())
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.Set("sss"+strconv.Itoa(i), "hello")
+	}
+}
+
 func BenchmarkSyncMap_Set(b *testing.B) {
 	var m = &sync.Map{}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		m.Store("sss"+strconv.Itoa(i), "hello")
+	}
+}
+
+func BenchmarkMapFNV_Get(b *testing.B) {
+	var m = New(WithFNVHash())
+	for i := 0; i < 10000; i++ {
+		m.Set(strconv.Itoa(i), "hello")
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.Get("sss" + strconv.Itoa(i))
+	}
+}
+
+func BenchmarkMapBKDR_Get(b *testing.B) {
+	var m = New(WithBKDRHash())
+	for i := 0; i < 10000; i++ {
+		m.Set(strconv.Itoa(i), "hello")
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.Get("sss" + strconv.Itoa(i))
+	}
+}
+
+func BenchmarkMapDJB_Get(b *testing.B) {
+	var m = New(WithDJBHash())
+	for i := 0; i < 10000; i++ {
+		m.Set(strconv.Itoa(i), "hello")
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.Get("sss" + strconv.Itoa(i))
+	}
+}
+
+func BenchmarkSyncMap_Get(b *testing.B) {
+	var m = &sync.Map{}
+	for i := 0; i < 10000; i++ {
+		m.Store("sss"+strconv.Itoa(i), "hello")
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.Load("sss" + strconv.Itoa(i))
 	}
 }
 
