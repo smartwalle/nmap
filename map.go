d367acf609f55df1195a7c3f28f79af3e746d977
smartwalle <smartwalle@gmail.com>
2023-10-16 14:03:01 +0800

    规范

--- map.go
+++ map.go
@@ -40,20 +40,20 @@ func NewMap[K Key, V any](sharding func(key K) uint32) *Map[K, V] {
 	return m
 }
 
-func (this *Map[K, V]) getShard(key K) *shardMap[K, V] {
-	var index = this.sharding(key)
-	return this.shards[index]
+func (m *Map[K, V]) getShard(key K) *shardMap[K, V] {
+	var index = m.sharding(key)
+	return m.shards[index]
 }
 
-func (this *Map[K, V]) Set(key K, value V) {
-	var shard = this.getShard(key)
+func (m *Map[K, V]) Set(key K, value V) {
+	var shard = m.getShard(key)
 	shard.Lock()
 	shard.elements[key] = value
 	shard.Unlock()
 }
 
-func (this *Map[K, V]) SetNx(key K, value V) bool {
-	var shard = this.getShard(key)
+func (m *Map[K, V]) SetNx(key K, value V) bool {
+	var shard = m.getShard(key)
 	shard.Lock()
 	var _, found = shard.elements[key]
 	if !found {
@@ -65,37 +65,37 @@ func (this *Map[K, V]) SetNx(key K, value V) bool {
 	return false
 }
 
-func (this *Map[K, V]) Get(key K) (V, bool) {
-	var shard = this.getShard(key)
+func (m *Map[K, V]) Get(key K) (V, bool) {
+	var shard = m.getShard(key)
 	shard.RLock()
 	var value, ok = shard.elements[key]
 	shard.RUnlock()
 	return value, ok
 }
 
-func (this *Map[K, V]) Exists(key K) bool {
-	var shard = this.getShard(key)
+func (m *Map[K, V]) Exists(key K) bool {
+	var shard = m.getShard(key)
 	shard.RLock()
 	var _, ok = shard.elements[key]
 	shard.RUnlock()
 	return ok
 }
 
-func (this *Map[K, V]) RemoveAll() {
-	for i := uint32(0); i < this.shardCount; i++ {
-		var shard = this.shards[i]
+func (m *Map[K, V]) RemoveAll() {
+	for i := uint32(0); i < m.shardCount; i++ {
+		var shard = m.shards[i]
 		shard.Lock()
 		shard.elements = make(map[K]V)
 		shard.Unlock()
 	}
 }
 
-func (this *Map[K, V]) Remove(key K) {
-	this.Pop(key)
+func (m *Map[K, V]) Remove(key K) {
+	m.Pop(key)
 }
 
-func (this *Map[K, V]) Pop(key K) (V, bool) {
-	var shard = this.getShard(key)
+func (m *Map[K, V]) Pop(key K) (V, bool) {
+	var shard = m.getShard(key)
 	shard.Lock()
 	var value, found = shard.elements[key]
 	if found {
@@ -105,10 +105,10 @@ func (this *Map[K, V]) Pop(key K) (V, bool) {
 	return value, found
 }
 
-func (this *Map[K, V]) Len() int {
+func (m *Map[K, V]) Len() int {
 	var count = 0
-	for i := uint32(0); i < this.shardCount; i++ {
-		var shard = this.shards[i]
+	for i := uint32(0); i < m.shardCount; i++ {
+		var shard = m.shards[i]
 		shard.RLock()
 		count += len(shard.elements)
 		shard.RUnlock()
@@ -116,12 +116,12 @@ func (this *Map[K, V]) Len() int {
 	return count
 }
 
-func (this *Map[K, V]) Range(f func(key K, value V) bool) {
+func (m *Map[K, V]) Range(f func(key K, value V) bool) {
 	if f == nil {
 		return
 	}
-	for i := uint32(0); i < this.shardCount; i++ {
-		var shard = this.shards[i]
+	for i := uint32(0); i < m.shardCount; i++ {
+		var shard = m.shards[i]
 		shard.RLock()
 		for k, v := range shard.elements {
 			shard.RUnlock()
@@ -134,10 +134,10 @@ func (this *Map[K, V]) Range(f func(key K, value V) bool) {
 	}
 }
 
-func (this *Map[K, V]) Elements() map[K]V {
-	var nMap = make(map[K]V, this.Len())
-	for i := uint32(0); i < this.shardCount; i++ {
-		var shard = this.shards[i]
+func (m *Map[K, V]) Elements() map[K]V {
+	var nMap = make(map[K]V, m.Len())
+	for i := uint32(0); i < m.shardCount; i++ {
+		var shard = m.shards[i]
 		shard.RLock()
 		for k, v := range shard.elements {
 			nMap[k] = v
@@ -147,10 +147,10 @@ func (this *Map[K, V]) Elements() map[K]V {
 	return nMap
 }
 
-func (this *Map[K, V]) Keys() []K {
-	var nKeys = make([]K, 0, this.Len())
-	for i := uint32(0); i < this.shardCount; i++ {
-		var shard = this.shards[i]
+func (m *Map[K, V]) Keys() []K {
+	var nKeys = make([]K, 0, m.Len())
+	for i := uint32(0); i < m.shardCount; i++ {
+		var shard = m.shards[i]
 		shard.RLock()
 		for k := range shard.elements {
 			nKeys = append(nKeys, k)
